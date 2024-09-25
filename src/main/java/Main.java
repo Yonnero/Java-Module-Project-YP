ef8eb893df5a082dd5b3d7a150cf3ffa92abcec5
Yonnero <trines63@yandex.com>
2024-09-25 11:42:31 +0400

    Revert "Выполнена проектная работа №1"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,6 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("*-------------------------------------------------------------------------*");
-        System.out.println(" Добро пожаловать на гонку '24 часа Ле-Мана!'");
-        System.out.println(" Вам необходимо ввести название и скорость автомобиля (от 1 до 250 км/ч).");
-        System.out.println(" В гонке будут соревноваться три участника.");
-        System.out.println(" После чего мы узнаем победителя гонки!");
-        System.out.println("*-------------------------------------------------------------------------*\n");
-
-        CompetitorsCreation competitorsCreation = new CompetitorsCreation();
-        competitorsCreation.createCompetitors();
-        new Race().printWinner(competitorsCreation.getCars());
+        System.out.println("Hello world!");
     }
 }
